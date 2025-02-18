9a1afd73f71bb8bae1a2c5a02e4ba0b76941612a
M4rl0n274 <marlonquinteri274@gmail.com>
2025-02-18 16:33:55 -0500

    creando entidad

--- backend_zafiro_azul/src/main/java/com/proyectofinal/backend_zafiro_azul/service/Iusuario_TemporalService.java
+++ backend_zafiro_azul/src/main/java/com/proyectofinal/backend_zafiro_azul/service/Iusuario_TemporalService.java
@@ -2,7 +2,10 @@ package com.proyectofinal.backend_zafiro_azul.service;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 public interface Iusuario_TemporalService {
+    
 
 
 }
