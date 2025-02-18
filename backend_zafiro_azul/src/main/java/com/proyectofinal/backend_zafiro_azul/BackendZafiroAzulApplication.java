bd8a47ff94e638d7f2dd68ee5ddb3e8653168c69
Torito9609 <toroandres85@gmail.com>
2025-02-18 15:45:05 -0500

    commit inicial

---
+++ backend_zafiro_azul/src/main/java/com/proyectofinal/backend_zafiro_azul/BackendZafiroAzulApplication.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.backend_zafiro_azul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendZafiroAzulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendZafiroAzulApplication.class, args);
+	}
+
+}
---
+++ backend_zafiro_azul/src/test/java/com/proyectofinal/backend_zafiro_azul/BackendZafiroAzulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.backend_zafiro_azul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendZafiroAzulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
