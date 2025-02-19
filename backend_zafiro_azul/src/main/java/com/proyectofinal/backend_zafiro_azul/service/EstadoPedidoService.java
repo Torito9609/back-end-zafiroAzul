6875fd9aa6c0137604d8e7db838fab772d7bd9bd
Torito9609 <toroandres85@gmail.com>
2025-02-19 09:39:46 -0500

    se termina estado pedido

--- backend_zafiro_azul/src/main/java/com/proyectofinal/backend_zafiro_azul/service/EstadoPedidoService.java
+++ backend_zafiro_azul/src/main/java/com/proyectofinal/backend_zafiro_azul/service/EstadoPedidoService.java
@@ -2,6 +2,7 @@ package com.proyectofinal.backend_zafiro_azul.service;
 
 import com.proyectofinal.backend_zafiro_azul.model.EstadoPedido;
 import com.proyectofinal.backend_zafiro_azul.repository.IEstadoPedidoRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,23 +19,31 @@ public class EstadoPedidoService implements IEstadoPedidoService{
         return estadoPedidoRepository.findAll();
     }
 
+    @Transactional
     @Override
     public void saveEstadoPedido(EstadoPedido estadoPedido) {
-
+        estadoPedidoRepository.save(estadoPedido);
     }
 
+    @Transactional
     @Override
     public void deleteEstadoPedido(Long idEstadoPedido) {
-
+        estadoPedidoRepository.deleteById(idEstadoPedido);
     }
 
     @Override
     public EstadoPedido findEstadoPedido(Long idEstadoPedido) {
-        return null;
+        return estadoPedidoRepository.findById(idEstadoPedido).orElse(null);
     }
 
+    @Transactional
     @Override
     public void editEstadoPedido(Long idEstadoPedidoToUpdate, EstadoPedido estadoPedidoUpdated) {
+        EstadoPedido estadoExistente = estadoPedidoRepository.findById(idEstadoPedidoToUpdate).orElse(null);
 
+        if(estadoExistente != null){
+            estadoExistente.setNombreEstado(estadoPedidoUpdated.getNombreEstado());
+            estadoPedidoRepository.save(estadoExistente);
+        }
     }
 }
